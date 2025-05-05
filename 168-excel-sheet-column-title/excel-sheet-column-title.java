2be6d6a2941c46b2bfe4727b44886f704677c7ba
Travis Keefe <travisjkeefe@gmail.com>
2025-05-05 16:53:05 -0700

    Time: 0 ms (100.00%) | Memory: 40.6 MB (83.50%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+        while (columnNumber > 0) {
+            columnNumber--;
+            result.insert(0, (char)('A' + columnNumber % 26));
+            columnNumber /= 26;
+        }
+        return result.toString();
+    }
+}
