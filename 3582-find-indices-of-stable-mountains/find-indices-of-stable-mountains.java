eb291a8ffd3cdd4f72c869c35102ab88e4f1e548
Travis Keefe <travisjkeefe@gmail.com>
2025-06-10 15:58:29 -0500

    Time: 1 ms (100.00%) | Memory: 44.9 MB (44.51%) - LeetSync

---
+++ 3582-find-indices-of-stable-mountains/find-indices-of-stable-mountains.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> stableMountains(int[] height, int threshold) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i < height.length; i++) {
+            if (height[i - 1] > threshold) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
