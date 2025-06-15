090ccfbd9553c42e99812985b7771ebaa729157d
Travis Keefe <travisjkeefe@gmail.com>
2025-06-15 12:39:11 -0500

    Time: 1 ms (100.00%) | Memory: 43.1 MB (41.01%) - LeetSync

---
+++ 3446-find-the-number-of-good-pairs-i/find-the-number-of-good-pairs-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int count = 0;
+        for (int num1 : nums1) {
+            for (int num2 : nums2) {
+                if (num1 % (num2 * k) == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
