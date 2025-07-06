8f2ab45f219f907d0a1b051e43501350ab515c92
Travis Keefe <travisjkeefe@gmail.com>
2025-07-06 14:52:07 -0700

    Time: 0 ms (100.00%) | Memory: 40.3 MB (98.40%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int total = n * (n + 1) / 2;
+
+        int x = (int) Math.sqrt(total);
+
+        if (x * x == total) {
+            return x;
+        } else {
+            return -1;
+        }
+    }
+}
